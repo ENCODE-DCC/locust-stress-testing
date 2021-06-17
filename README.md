e44387d723bf8414e4a269fa55774fb376bc1408
ottojolanki <ojolanki@stanford.edu>
2021-06-16 17:11:55 -0700

    DEV-60 add logstreamer class and requirements plus short readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # locust-stress-testing
 Send requests parsed from apache access log
+
+## Prepare logs
+
+The file that will be used as the source of the requests is a text file with one route per line. It can be parsed from apache `access.log` file using `awk` in the following way:
+
+```
+cat access.log | awk '{pring $9}' > request_source.txt
+```
+The actual stress testing will randomly choose a lines from the above file.
---
+++ logstream.py
@@ -0,0 +1,40 @@
+import mmap
+import numpy as np
+
+from itertools import cycle
+
+
+class LogStream:
+    def __init__(
+        self, source_file: str, offsets=None, shuffle: bool = True, seed: int = 42
+    ):
+        self.source_file = source_file
+        if offsets:
+            print("use given offsets")
+        else:
+            print("scan offsets")
+            offsets = self.scan_offsets()
+        self.offsets = offsets
+        if shuffle:
+            np.random.shuffle(self.offsets)
+        # we want an inexhaustible source of offsets
+        self.offsets = cycle(self.offsets)
+
+    def __iter__(self):
+        with open(self.source_file, "r+b") as f:
+            mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            for position in self.offsets:
+                mm.seek(position)
+                record = mm.readline()
+                yield record
+
+    def scan_offsets(self):
+        tmp_offsets = []
+        with open(self.source_file, "r+b") as f:
+            mm = mmap.mmap(f.fileno(), 0, access=mmap.ACCESS_READ)
+            for _ in iter(mm.readline, b""):
+                position = mm.tell()
+                tmp_offsets.append(position)
+        offsets = np.asarray(tmp_offsets, dtype="uint64")
+        del tmp_offsets
+        return offsets
